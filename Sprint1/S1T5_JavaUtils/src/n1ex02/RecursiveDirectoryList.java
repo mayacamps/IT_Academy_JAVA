b32bc8b52314dadb59820590a87a21d2b1e60059
Maya <maya.camps@estudiantat.upc.edu>
2023-10-05 18:29:30 +0200

    05/10 s1t5 n1ex02 loop for recursive listing

--- Sprint1/S1T5_JavaUtils/src/n1ex02/RecursiveDirectoryList.java
+++ Sprint1/S1T5_JavaUtils/src/n1ex02/RecursiveDirectoryList.java
@@ -1,5 +1,7 @@
 package n1ex02;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Scanner;
 
@@ -16,6 +18,71 @@ public class RecursiveDirectoryList {
     }
 
     public void getRecursiveListContent(String path){
+        File dir = new File(path);
 
+        if (!dir.exists() || !dir.isDirectory()){
+            System.out.println("Path given is not directory.");
+            return;
+        }
+
+        boolean thereIsFolder = false;
+        do {
+            File [] content = dir.listFiles();
+            if (content.length == 0){
+                System.out.println("Directory is empty.");
+            } else{
+                Arrays.sort(content);
+
+                for (File s : content) {
+                    if (s.isDirectory()) {
+                        thereIsFolder = true;
+                        System.out.println(s.getName() + "(D)");
+
+                        File[] folder = s.listFiles();
+                        for (File a : folder) {
+                            if (a.isDirectory()) {
+                                System.out.println("\t" + a.getName() + "(D)");
+                            } else {
+                                System.out.println("\t" + a.getName() + "(F)");
+                            }
+                        }
+                    } else {
+                        System.out.println(s.getName() + "(F)");
+                    }
+                }
+//                File[] folder = s.listFiles();
+//                for (File a : folder) {
+
+                }
+
+        } while (!thereIsFolder);
+
+
+//        File [] content = dir.listFiles();
+//
+//        if (content.length == 0){
+//            System.out.println("Directory is empty.");
+//        } else{
+//            Arrays.sort(content);
+//            for (File s : content) {
+//                if (s.isDirectory()) {
+//                    thereIsFolder = true;
+//                    System.out.println(s.getName() + "(D)");
+//
+//                    File[] folder = s.listFiles();
+//                    for (File a : folder) {
+//                        if (a.isDirectory()) {
+//                            System.out.println("\t" + a.getName() + "(D)");
+//                        } else {
+//                            System.out.println("\t" + a.getName() + "(F)");
+//                        }
+//                    }
+//                } else {
+//                    System.out.println(s.getName() + "(F)");
+//                }
+//            }
+
+            
     }
 }
+
