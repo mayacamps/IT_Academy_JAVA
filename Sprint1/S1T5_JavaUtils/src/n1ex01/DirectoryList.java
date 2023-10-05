8d0a99c49b4a5085073a4cbf753d6e7706f9ff9d
Maya <maya.camps@estudiantat.upc.edu>
2023-10-05 17:20:11 +0200

    5/10 s1t5 n1ex01 done

--- Sprint1/S1T5_JavaUtils/src/n1ex01/DirectoryList.java
+++ Sprint1/S1T5_JavaUtils/src/n1ex01/DirectoryList.java
@@ -1,5 +1,39 @@
 package n1ex01;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class DirectoryList {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String path;
+
+        System.out.println("This program lists all the contents of a specific directory.\n" +
+                "Please introduce the directory's path: ");
+        path = sc.nextLine();
+
+        new DirectoryList().getListContent(path);
+
+    }
+
+    public void getListContent(String path){
+        File file = new File(path);
+
+        if (!file.exists() || !file.isDirectory()){
+            System.out.println("Path given is not directory.");
+            return;
+        }
+
+        String [] content = file.list();
+
+        if (content.length == 0){
+            System.out.println("Directory is empty.");
+        } else{
+            for (String s : content) {
+                System.out.println(s);
+            }
+        }
+
+    }
 }
