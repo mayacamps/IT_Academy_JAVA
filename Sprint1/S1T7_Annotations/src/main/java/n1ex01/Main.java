68460f002bf9847799b0ac3e843fa756f66be5ff
Maya <maya.camps@estudiantat.upc.edu>
2023-10-16 11:49:04 +0200

    tested override method in main

--- Sprint1/S1T7_Annotations/src/main/java/n1ex01/Main.java
+++ Sprint1/S1T7_Annotations/src/main/java/n1ex01/Main.java
@@ -2,6 +2,12 @@ package n1ex01;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Worker w1 = new Worker("Elena", "Nito", 12.5d);
+        Worker w2 = new OnlineWorker("Marco", "Polo", 7);
+        Worker w3 = new InPersonWorker("Celia", "Cruz", 20.5d);
+
+        w1.calcSalary(10);
+        w2.calcSalary(10);
+        w3.calcSalary(10);
     }
 }
