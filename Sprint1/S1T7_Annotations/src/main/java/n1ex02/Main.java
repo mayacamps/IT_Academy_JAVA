081eca5876b33637757f281dc5510af13e93a45a
Maya Camps <127402122+mayacamps@users.noreply.github.com>
2023-10-17 10:13:31 +0200

    applied warning suppression

--- Sprint1/S1T7_Annotations/src/main/java/n1ex02/Main.java
+++ Sprint1/S1T7_Annotations/src/main/java/n1ex02/Main.java
@@ -1,14 +1,20 @@
 package n1ex02;
 
 public class Main {
+    @SuppressWarnings("deprecation")
+    static void printOnlineWorkerMessage(OnlineWorker worker){
+        worker.deprecatedMethod();
+    }
+    @SuppressWarnings("deprecation")
+    static void printInPersonWorkerMessage(InPersonWorker worker){
+        worker.deprecatedMethod();
+    }
     public static void main(String[] args) {
-        @SuppressWarnings({"deprecation", "RedundantSuppression"})
-                
         Worker w1 = new Worker("Elena", "Nito", 12.5d);
         OnlineWorker w2 = new OnlineWorker("Marco", "Polo", 7);
         InPersonWorker w3 = new InPersonWorker("Celia", "Cruz", 20.5d);
 
-        w2.deprecatedMethod();
-
+        printOnlineWorkerMessage(w2);
+        printInPersonWorkerMessage(w3);
     }
 }
