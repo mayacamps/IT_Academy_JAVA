63d5d69db1f9a24962ca7b7250b92925532ecac8
Maya Camps <127402122+mayacamps@users.noreply.github.com>
2023-10-17 10:44:09 +0200

    tested generic method

--- Sprint1/S1T6_Generics/src/n1ex02/Main.java
+++ Sprint1/S1T6_Generics/src/n1ex02/Main.java
@@ -3,7 +3,11 @@ package n1ex02;
 public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Mark", "Wahlberg", 34);
-        
-        GenericMethods.printAll(person1, "Test text", 111);
+        String text = "Test text";
+        int num = 111;
+
+        GenericMethods.printAll(person1, text, num);
+        GenericMethods.printAll(text, num, person1);
+        GenericMethods.printAll(num, person1, text);
     }
 }
