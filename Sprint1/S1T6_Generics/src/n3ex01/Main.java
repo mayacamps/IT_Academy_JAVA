b80fc3cc8dd247bdf2bba1a7c2b44185c232830e
Maya Camps <127402122+mayacamps@users.noreply.github.com>
2023-10-18 10:11:08 +0200

    s1t6 finished

--- Sprint1/S1T6_Generics/src/n3ex01/Main.java
+++ Sprint1/S1T6_Generics/src/n3ex01/Main.java
@@ -1,4 +1,10 @@
 package n3ex01;
 
 public class Main {
+    public static void main(String[] args) {
+        Smartphone smartphone = new Smartphone("Samsung", "Galaxy");
+        Generic.genericPhone(smartphone);
+        Generic.genericSmartphone(smartphone);
+    }
+
 }
