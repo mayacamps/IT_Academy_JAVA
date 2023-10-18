d5cf6938d73b86e7f0c34907b28f05bc9a85f533
Maya Camps <127402122+mayacamps@users.noreply.github.com>
2023-10-18 10:36:38 +0200

    imported entry.readint method

--- Sprint1/S1T2_Exceptions/src/n3ex01/Main.java
+++ Sprint1/S1T2_Exceptions/src/n3ex01/Main.java
@@ -1,33 +1,27 @@
 package n3ex01;
 
-import java.util.InputMismatchException;
+import n2ex01.Entry;
 import java.util.Scanner;
 
 public class Main {
-
 	static Scanner en = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
+
 		int numRows, numSeats, menuOpt;
-	
-		System.out.println("How many rows does the cinema room have?");
-		numRows = getInt();
-		
-		System.out.println("How many seats does the cinema room have?");
-		numSeats = getInt();
+
+		numRows = Entry.readInt("How many rows does the cinema room have?");
+		numSeats  = Entry.readInt("How many seats does the cinema room have?");
 		
 		do {
-			System.out.println("Cinema menu:\n"
+			menuOpt = Entry.readInt("Cinema menu:\n"
 					+ "1. Show all reserved seats.\n"
 					+ "2. Show reserved seats for one person.\n"
 					+ "3. Reserve seat.\n"
 					+ "4. Cancel seat reservation.\n"
 					+ "5. Cancel all reservations for one person.\n"
 					+ "0. Exit.\n ");
-			
-			menuOpt = getInt();
-			
+
 			switch(menuOpt) {
 			case 1:
 				break;
@@ -49,22 +43,6 @@ public class Main {
 		} while (menuOpt != 0);
 		
 	}
-	
-	public static int getInt() {
-		int num = 0;
-		boolean isInt = false;
-		
-		do {
-			try {
-				num = en.nextInt();
-				isInt = true;
-			} catch (InputMismatchException e){
-				System.err.println("InputMismatchException: Please introduce a valid number.");
-			}
-			en.nextLine();
-		} while (!isInt);
-	
-		return num;
-	}
+
 }
 
