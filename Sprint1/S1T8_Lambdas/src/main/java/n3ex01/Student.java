e67c2e6d7d69947b8ace73aae8e7990cf222cf19
Maya <maya.camps@estudiantat.upc.edu>
2023-10-23 12:08:24 +0200

    created Student class

---
+++ Sprint1/S1T8_Lambdas/src/main/java/n3ex01/Student.java
@@ -0,0 +1,56 @@
+package n3ex01;
+
+public class Student {
+    private String name, course;
+    private int age;
+    private float score;
+
+    public Student(String name, String course, int age, float score) {
+        this.name = name;
+        this.course = course;
+        this.age = age;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course='" + course + '\'' +
+                ", age=" + age +
+                ", score=" + score +
+                '}';
+    }
+}
