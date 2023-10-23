42cb332c0d1c0349bf09b7463c5f95814e09f930
Maya <maya.camps@estudiantat.upc.edu>
2023-10-23 09:22:21 +0200

    n1ex07 reverse ordering

---
+++ Sprint1/S1T8_Lambdas/src/main/java/n1ex07/Main.java
@@ -0,0 +1,15 @@
+package n1ex07;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Object> list = Arrays.asList("Sunflower", "Daisy", 2312, "Lilly", 98273, 12, "Rose", 9);
+
+        list.stream()
+                .sorted(Comparator.comparing(l -> -(l.toString().length())))
+                .forEach(System.out::println);
+    }
+}
