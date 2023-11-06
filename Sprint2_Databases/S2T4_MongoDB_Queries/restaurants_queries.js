0fbce9fe10af125fe22a59dbe96d253f64229e30
Maya <maya.camps@estudiantat.upc.edu>
2023-11-06 14:15:12 +0100

    done mongodb restaurants queries

--- Sprint2_Databases/S2T4_MongoDB_Queries/restaurants_queries.js
+++ Sprint2_Databases/S2T4_MongoDB_Queries/restaurants_queries.js
@@ -40,22 +40,22 @@ db.restaurants.find({cuisine: {$not : /^American/}, "grades.score" : {"$gt" : 70
 db.restaurants.find({cuisine: {$not : /^American/}, "grades.grade" : "A", borough : {$ne :"Brooklyn"}})
 
 //14 - Find the restaurant_id, name, borough and cuisine for those restaurants that contain "Wil" as the first three letters in their name
-db.restaurants.find({name: /^Wil/}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
+db.restaurants.find({name: /^Wil/i}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
 
 //15 - Find the restaurant_id, name, borough and cuisine for those restaurants that contain "ces" as the last three letters in their name
 db.restaurants.find({name: /ces$/}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
 
 //16 - Find the restaurant_id, name, borough and cuisine for those restaurants that contain "Reg" as three letters somewhere in their name
-db.restaurants.find({name: /.*Reg.*/}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
+db.restaurants.find({name: /Reg/i}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
 
 //17 - Find the restaurants that belong to the Bronx and prepared any American or Chinese dishes
-db.restaurants.find({$and : [{borough : "Bronx"}, {$or : [{cuisine : /^American/}, {cuisine: "Chinese"}]}]})
+db.restaurants.find({borough : "Bronx", cuisine: { $in: [/^American/, "Chinese"]}})
 
 //18 - Find the restaurant_id, name, borough and cuisine for those restaurants that belong to Staten Island or Queens or Bronx or Brooklyn
 db.restaurants.find({borough: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
 
 //19 - Find the restaurant_id, name, borough and cuisine for restaurants outside Staten Island or Queens or Bronx or Brooklyn
-db.restaurants.find({borough: {$not : { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
+db.restaurants.find({borough: {$nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
 
 //20 - Find the restaurant_id, name, borough and cuisine for restaurants that get a score that"s no more than 10
 db.restaurants.find({"grades.score" : {$lte : 10}}, {restaurant_id : 1, name : 1, borough : 1, cuisine : 1})
@@ -63,14 +63,24 @@ db.restaurants.find({"grades.score" : {$lte : 10}}, {restaurant_id : 1, name : 1
 //22 - Find the restaurant_id, name, and stands for those restaurants that achieve an "A" grade and an 11 score in ISODate study data "2014-08-11T00:00:00Z"
 db.restaurants.find({grades: {$elemMatch: {grade : "A", score : 11, date : ISODate("2014-08-11T00:00:00Z")}}}, {restaurant_id : 1, name : 1, grades : 1})
 
-//23 - Find the restaurant_id, name and stands for those restaurants where the 2nd grade variety item contains a grade of "A" and score 9 on an ISODate "2014-08-11T00:00:00Z"
-db.restaurants.find({"grades.1": {$elemMatch: {grade : "A", score : 9, date : ISODate("2014-08-11T00:00:00Z")}}}, {restaurant_id : 1, name : 1, grades : 1})
+//23 - Find the restaurant_id, name and grades for those restaurants where the 2nd grade variety item contains a grade of "A" and score 9 on an ISODate "2014-08-11T00:00:00Z"
+db.restaurants.find({"grades.1.grade" : "A", "grades.1.score" : 9, "grades.1.date" : ISODate("2014-08-11T00:00:00Z")}, {restaurant_id : 1, name : 1, grades : 1})
 
-db.restaurants.aggregate([
-    { $project: 
-    {
-        restaurant_id : 1, 
-        name : 1, 
-        grade : {$arrayElemAt : ["$grades", 1]} }}
-        , {$match : {grade : "A", score : 9, date : ISODate("2014-08-11T00:00:00Z")}}
-])
+//24 - Find the restaurant_id, name, address, and geographic location for restaurants where the second element of the array coord contains a value that is greater than 42 and up to 52
+db.restaurants.find({"address.coord.1" : {$gt : 42, $lte : 52}}, {restaurant_id : 1, name : 1, address : 1, borough : 1})
+
+//25 - Organize your restaurant name in ascending order along with all columns
+db.restaurants.find().sort({name : 1})
+
+//26 - Organize your restaurant name in descending order along with all columns
+db.restaurants.find().sort({name : -1})
+
+//27 - Organize the name of the cuisine in ascending order and by the district itself in descending order
+db.restaurants.find().sort({cuisine : 1, borough : -1})
+
+//28 - Find out all directions that don't contain the street
+db.restaurants.find({"address.street" : {$exists : false}})
+db.restaurants.find({"address.street" : null})
+
+//29 - Select all documents in the collection of restaurants where the value of the coord field is Double
+db.restaurants.find({"address.coord":{$type: "double"}})
