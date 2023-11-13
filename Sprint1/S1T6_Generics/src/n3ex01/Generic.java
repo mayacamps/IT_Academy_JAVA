0032a8a692f1ad97c3e2c3da420aa0be5acec1b6
Maya <maya.camps@estudiantat.upc.edu>
2023-11-13 13:15:23 +0100

    correction: updated generic methods

--- Sprint1/S1T6_Generics/src/n3ex01/Generic.java
+++ Sprint1/S1T6_Generics/src/n3ex01/Generic.java
@@ -1,12 +1,12 @@
 package n3ex01;
 
 public class Generic {
-    public static <T> void genericPhone(Phone phone){
-        phone.ring();
+    public static <T extends Phone> void genericPhone(T t){
+        t.ring();
     }
 
-    public static <T> void genericSmartphone(Smartphone smartphone){
-        smartphone.ring();
-        smartphone.takePhotos();
+    public static <T extends Smartphone> void genericSmartphone(T t){
+        t.ring();
+        t.takePhotos();
     }
 }
